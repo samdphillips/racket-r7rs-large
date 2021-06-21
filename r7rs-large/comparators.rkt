28766d4707b6cceb0c4a81c1fc6956eab272f56b
Sam Phillips <samdphillips@gmail.com>
2021-06-20 21:55:17 -0700

    (scheme comparators)
    Add comparators from SRFI-128 reference implementation and tests.
    Modifications:
    - formatting
    - use Racket specific values for `(hash-bound)`
    - initialize the salt with a random value

---
+++ r7rs-large/comparators.rkt
@@ -0,0 +1,25 @@
+#lang r7rs
+
+(define-library (scheme comparators)
+  (import (scheme base))
+  (import (scheme case-lambda))
+  (import (scheme char) (scheme complex) (scheme inexact))
+  (import (only (racket base)
+                arithmetic-shift bitwise-and bitwise-ior random sub1
+                system-type))
+  (export comparator? comparator-ordered? comparator-hashable?)
+  (export make-comparator
+          make-pair-comparator make-list-comparator make-vector-comparator
+          make-eq-comparator make-eqv-comparator make-equal-comparator)
+  (export boolean-hash char-hash char-ci-hash
+          string-hash string-ci-hash symbol-hash number-hash)
+  (export make-default-comparator default-hash comparator-register-default!)
+  (export comparator-type-test-predicate comparator-equality-predicate
+        comparator-ordering-predicate comparator-hash-function)
+  (export comparator-test-type comparator-check-type comparator-hash)
+  (export hash-bound hash-salt)
+  (export =? <? >? <=? >=?)
+  (export comparator-if<=>)
+  (include "private/include/comparators/comparators-impl.scm")
+  (include "private/include/comparators/default.scm"))
+
