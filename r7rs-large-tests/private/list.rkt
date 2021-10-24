b6ad02cdd8a9dcd5fb89d0d2b93d5c9f6e61b3b8
Sam Phillips <samdphillips@gmail.com>
2021-10-23 17:07:50 -0700

    Add more tests

--- r7rs-large-tests/private/list.rkt
+++ r7rs-large-tests/private/list.rkt
@@ -20,3 +20,37 @@
 
 (test (make-list 4 'c) '(c c c c))
 
+(test (list-tabulate 4 values) '(0 1 2 3))
+
+(let ((cl (circular-list 'z 'q)))
+  (test (list-ref cl 0) 'z)
+  (test (list-ref cl 1) 'q)
+  (test (list-ref cl 2) 'z)
+  (test (list-ref cl 3) 'q)
+  (test-assert (circular-list? cl)))
+
+(test (iota 5) '(0 1 2 3 4))
+(test (iota 5 0 -1/10) '(0 -1/10 -2/10 -3/10 -4/10))
+
+(test (pair? '(a . b)) #t)
+(test (pair? '(a b c)) #t)
+(test (pair? '())      #f)
+(test (pair? '#(a b))  #f)
+(test (pair? 7)        #f)
+(test (pair? 'a)       #f)
+
+(test-assert (list= eq?))
+(test-assert (list= eq? '(a)))
+(test-assert (list= = '(1 2 3) '(1 2 3)))
+(test-assert (list= eq? '(a b c) '(a b c)))
+
+(let ()
+  (call-with-values
+    (lambda () (car+cdr '(a b)))
+    (lambda (x y)
+      (test x 'a)
+      (test y '(b)))))
+
+(test (take '(a b c d e)  2) '(a b))
+(test (drop '(a b c d e)  2) '(c d e))
+
